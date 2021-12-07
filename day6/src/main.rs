e6e794b2d4a245d858b88294393d965acf9ab472
Shing Ho Tan <talenteyedev@gmail.com>
2021-12-06 16:57:35 -0800

    change to biguint

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -27,26 +27,30 @@
 //     println!("{}", nums.len());
 // }
 use std::fs;
+use num_bigint::BigUint;
+use num_traits::{Zero, One}; // 0.2.8
 
 fn main() {
     let content = fs::read_to_string("input1.txt").expect("something went wrong");
     let splited_line: Vec<&str> = content.split(",").collect();
-    let mut nums: Vec<i128> = vec![0; 9];
+    let zero: BigUint = Zero::zero();
+    let mut nums: Vec<BigUint> = vec![zero; 9];
 
     for each in splited_line {
         let num: usize = each.parse().unwrap();
-        nums[num] = nums[num] + 1;
+        let one: BigUint = One::one();
+        nums[num] += one;
     }
 
-    for _i in 0..256 {
-        let zero_count = nums[0];
+    for _i in 0..100000 {
+        let zero_count: BigUint = nums[0].clone();
         nums.rotate_left(1);
-        nums[6] = nums[6] + zero_count;
+        nums[6] += zero_count;
     }
 
-    let mut sum = 0;
+    let mut sum: BigUint = Zero::zero();
     for i in 0..9 {
-        sum = sum + nums[i];
+        sum += &nums[i];
     }
 
     println!("{}", sum);
